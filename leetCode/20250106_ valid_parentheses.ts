0f8ab9a8542e47c40c2deb3c5f5395fc49f6ae6b
KAJI-JAPAN <kurowassan1206@gmail.com>
2026-01-06 00:07:50 +0900

    add:方針を追加

---
+++ leetCode/20250106_ valid_parentheses.ts
@@ -0,0 +1,14 @@
+/*
+1) 開き括弧は stack に積む
+2) 閉じ括弧は stack の一番上と対応している必要がある
+3) 最後に stack が空なら true
+
+falseの条件
+・閉じ括弧がきたときにstackが空
+・stackが空ではない
+・pop した開き括弧が、閉じ括弧と種類不一致
+ */
+
+function isValid(s: string): boolean {
+
+};
