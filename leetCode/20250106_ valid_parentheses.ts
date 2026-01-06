eb49f1780bb8ff9305650c326c69fa0544cfc8cd
KAJI-JAPAN <kurowassan1206@gmail.com>
2026-01-07 08:56:09 +0900

    add:20260107

--- leetCode/20250106_ valid_parentheses.ts
+++ leetCode/20250106_ valid_parentheses.ts
@@ -5,10 +5,51 @@
 
 falseの条件
 ・閉じ括弧がきたときにstackが空
-・stackが空ではない
+・s最後にtackが空ではない
 ・pop した開き括弧が、閉じ括弧と種類不一致
  */
-
 function isValid(s: string): boolean {
+    const pair = {
+        ")":"(",
+        "}":"{",
+        "]":"["
+    }
+    let stack = ["("]
+    for(const string of s) {
+        // pushしてcontinueにすることで、以降を発火させない
+        if(string === "(" || string === "{" || string === "[") {
+            stack.push(string)
+            continue
+        }
+        // 以降は閉じ括弧の処理
+        if(stack.length === 0) return false
 
+        const top = stack.pop()
+        if(top !== pair[string]) return false
+}
+
+    return stack.length === 0
 };
+
+
+/*
+* Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+Example 4:
+Input: s = "([])"
+Output: true
+
+Example 5:
+Input: s = "([)]"
+Output: false
+* */
