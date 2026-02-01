61b09fb7c332f9becbfa3f54c73745902b5ed289
KAJI-JAPAN <kurowassan1206@gmail.com>
2026-02-01 19:36:54 +0900

    add:map group

--- leetCode/20260201_GroupAnagrams.ts
+++ leetCode/20260201_GroupAnagrams.ts
@@ -1,3 +1,26 @@
+// アナグラム = 同じ文字、同じ回数
+// なので、sortして同じものをグループ化することで分けて作っていく
+// eat → sortする → aet sortは昇順でアルファベット順になる
+// tea → sortする → aet sortでアルファベット順になるので同じになる
+// 上記、同じ値をグループ化する
+// これはMapでやる
+//  グループ化したい
+//  “検索” を大量にする
+/
 function groupAnagrams(strs: string[]): string[][] {
+	let groupMap: Map<string, string[]> = new Map()
 
+	for(const char of strs) {
+		const chars = char.split("")
+		chars.sort()
+
+		const key = chars.join()
+
+		if(!groupMap.has(key)) {
+			groupMap.set(key, [])
+		}
+		groupMap.get(key)!.push(char)
+	}
+
+	return Array.from(groupMap.values())
 };
