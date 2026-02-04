1246ccbd16eff191ef5a478403754ff68148d39b
KAJI-JAPAN <kurowassan1206@gmail.com>
2026-02-04 09:27:16 +0900

    add:BFS

---
+++ leetCode/20260204_BinaryTreeLevelOrderTraversal.ts
@@ -0,0 +1,33 @@
+ // * Definition for a binary tree node.
+  class TreeNode {
+      val: number
+      left: TreeNode | null
+      right: TreeNode | null
+      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.left = (left===undefined ? null : left)
+          this.right = (right===undefined ? null : right)
+      }
+ }
+
+function levelOrder(root: TreeNode | null): number[][] {
+	if(root === null) return []
+
+	let res: number[][] = []
+	let queue: TreeNode[] = [root]
+
+	while(queue.length > 0) {
+		const levelSize = queue.length
+		let level: number[] = []
+
+		for(let i=0; i < levelSize; i++) {
+			const node = queue.shift()
+			level.push(node.val)
+
+			if(node.left) queue.push(node.left)
+			if(node.right) queue.push(node.right)
+		}
+		res.push(level)
+	}
+	return res
+};
